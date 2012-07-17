2605a6168c9af5d94a0a45b5345e3ebc43e0e5d2
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-07-17 16:47:04 +0200

    Adding clojure-to-ruby-hash conversion

--- lib/zweikopf/hash.rb
+++ lib/zweikopf/hash.rb
@@ -1,4 +1,5 @@
 java_import "clojure.lang.PersistentHashMap"
+java_import "clojure.lang.PersistentArrayMap"
 java_import "clojure.lang.Keyword"
 java_import "java.util.Map"
 
@@ -12,7 +13,7 @@ module Zweikopf
       ret = empty.as_transient
       hash.each do |k, v|
         if v.is_a?(::Hash)
-          ret = ret.assoc(Keyword.intern(k.to_s), create(v))
+          ret = ret.assoc(Keyword.intern(k.to_s), self.create(v))
         else
           ret = ret.assoc(Keyword.intern(k.to_s), v)
         end
@@ -20,5 +21,20 @@ module Zweikopf
       ret.persistent
     end
 
+    def self.from_clj(clj_hash)
+      {}.tap do |ruby_map|
+        clj_hash.each do |k, v|
+          if v.is_a?(PersistentHashMap) || v.is_a?(PersistentArrayMap)
+            ruby_map[clj_to_ruby_symbol(k)] = self.from_clj(v)
+          else
+            ruby_map[clj_to_ruby_symbol(k)] = v
+          end
+        end
+      end
+    end # self.from_clj
+
+    def self.clj_to_ruby_symbol(keyword)
+      keyword.to_s.gsub(/:/, "").to_sym
+    end # self.clj_to_ruby_symbol
   end
 end
--- spec/zweikopf/hash_spec.rb
+++ spec/zweikopf/hash_spec.rb
@@ -30,6 +30,7 @@ describe Zweikopf::Hash do
     end
 
     context "when given a recursive hash structure" do
+
       #
       # Environment
       #
@@ -42,6 +43,59 @@ describe Zweikopf::Hash do
 
       it "creates a Clojure hash" do
         Zweikopf::Hash.create({:a => 1, :b => {:c => 3, :d =>4}}).should eql hash
+      end
+    end
+  end
+
+  describe :from_clj do
+    context "given a non-recursive clojure hash structure" do
+
+      #
+      # Environment
+      #
+
+      let(:clj_hash) { load_fixture(:clj_hash1) }
+
+      #
+      # Examples
+      #
+
+      it "creates a Ruby hash" do
+        Zweikopf::Hash.from_clj(clj_hash).should eql({:a => 1, :b => 2})
+      end
+    end
+
+    context "when given a recursive hash structure" do
+
+      #
+      # Environment
+      #
+
+      let(:clj_hash) { load_fixture(:clj_deep_hash1) }
+
+      #
+      # Examples
+      #
+
+      it "creates a Clojure hash" do
+        Zweikopf::Hash.from_clj(clj_hash).should eql({:a => 1, :b => {:c => 3, :d =>4}})
+      end
+    end
+
+    context "when given a clojure array" do
+
+      #
+      # Environment
+      #
+
+      let(:clj_array) { load_fixture(:clj_array1) }
+
+      #
+      # Examples
+      #
+
+      it "creates a Ruby array" do
+        #
       end
     end
   end
