a66775a40e6c682661e07ed36b824f52946c8edc
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-07-18 11:07:02 +0200

    Removing jdk6, fixing performance specs

--- spec/zweikopf/performance_spec.rb
+++ spec/zweikopf/performance_spec.rb
@@ -4,7 +4,7 @@ describe :performance do
   it "this spec simply shows performance" do
     time_before = Time.now.to_f
     100000.times do |i|
-      Zweikopf::Hash.create({:a => i, :b => i})
+      Zweikopf::Hash.from_ruby({:a => i, :b => i})
     end
     puts "Time elapsed: #{Time.now.to_f - time_before} seconds"
   end
