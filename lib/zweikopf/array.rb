dbcdf638cef08dbfb42555e7436a666c4f15f79b
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-09-24 12:59:12 +0200

    Adding block conversion to array

--- lib/zweikopf/array.rb
+++ lib/zweikopf/array.rb
@@ -3,16 +3,16 @@ java_import "clojure.lang.PersistentVector"
 module Zweikopf
   module Array
 
-    def self.from_ruby(arr)
+    def self.from_ruby(arr, &block)
       PersistentVector.create(arr.inject([]) do |acc, v|
-                                acc<< Zweikopf::Transformer.from_ruby(v)
+                                acc<< Zweikopf::Transformer.from_ruby(v, &block)
                               end)
     end # to_persistent_vector
 
-    def self.from_clj(clj_arr)
+    def self.from_clj(clj_arr, &block)
       [].tap do |ruby_arr|
         clj_arr.each do |v|
-          ruby_arr<< Zweikopf::Transformer.from_clj(v);
+          ruby_arr<< Zweikopf::Transformer.from_clj(v, &block);
         end
       end
     end # self.from_clj
