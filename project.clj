2c0bd942f44372ed4c24c004919ccf4fc2e8f591
Alex Petrov <alexp@coffeenco.de>
2014-01-27 18:10:41 +0100

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zweikopf "0.4.0"
+(defproject zweikopf "1.0.0-SNAPSHOT"
   :description "jruby clojure interop"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.jruby/jruby-complete "1.7.1"]]
