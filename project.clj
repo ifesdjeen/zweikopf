3fc966cc8f03002e100247cbdc2ebb8eaa109477
Alex Petrov <alexp@coffeenco.de>
2014-01-28 15:44:42 +0100

    Release 1.0.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zweikopf "1.0.0-SNAPSHOT"
+(defproject zweikopf "1.0.0-beta1"
   :description "jruby clojure interop"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.jruby/jruby-complete "1.7.1"]]
