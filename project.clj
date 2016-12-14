66e427e77320f073d698581412603894d98f07d9
Christopher Mark Gore <chris.gore@climate.com>
2016-12-14 11:11:01 -0600

    Clojure 1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject zweikopf "1.0.1"
   :description "jruby clojure interop"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jruby/jruby-complete "1.7.10"]])
