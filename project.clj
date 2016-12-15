bbfbc39d84db4310515c0debddb7d5726d740700
Alex Petrov <oleksandr.petrov@gmail.com>
2016-12-15 21:59:22 +0100

    Release 1.0.1 Release 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zweikopf "1.0.1"
+(defproject zweikopf "1.0.2"
   :description "jruby clojure interop"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jruby/jruby-complete "9.1.6.0"]])
